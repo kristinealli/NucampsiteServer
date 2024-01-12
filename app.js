22c7f0ecaca0b013a8af17c619ec0d1d79368aa6
Kristine Johnson <kristineallisonjohnson@outlook.com>
2024-01-11 22:40:03 -0500

    Express Session Part 2

--- app.js
+++ app.js
@@ -46,36 +46,19 @@ app.use(
 	})
 );
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 // Authentication Middleware
 function auth(req, res, next) {
 	console.log(req.session); //req.session is an object that contains session info
 
 	if (!req.session.user) {
-		//check if user is already authenticated
-		const authHeader = req.headers.authorization;
-		if (!authHeader) {
 			const err = new Error("You are not authenticated!");
-			res.setHeader("WWW-Authenticate", "Basic");
 			err.status = 401;
 			return next(err);
-		}
-
-		const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-			.toString()
-			.split(":");
-		const user = auth[0];
-		const pass = auth[1];
-		if (user === "admin" && pass === "password") {
-			req.session.user = "admin";
-			return next(); // authorized
-		} else {
-			const err = new Error("You are not authenticated!");
-			res.setHeader("WWW-Authenticate", "Basic");
-			err.status = 401;
-			return next(err);
-		}
 	} else {
-		if (req.session.user === "admin") {
+		if (req.session.user === "authenticated") {
 			return next();
 		} else {
 			const err = new Error("You are not authenticated!");
@@ -89,8 +72,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
