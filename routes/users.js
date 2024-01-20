9e8253d9208136460f35c17964de783598046e22
Kristine Johnson <kristineallisonjohnson@outlook.com>
2024-01-20 09:29:09 -0500

    OAuth2.0

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,23 @@ const cors = require("./cors");
 const router = express.Router();
 
 router.get(
+	"/facebook/token",
+	passport.authenticate("facebook-token"),
+	(req, res) => {
+		if (req.user) {
+			const token = authenticate.getToken({ _id: req.user._id });
+			res.statusCode = 200;
+			res.setHeader("Content-Type", "application/json");
+			res.json({
+				success: true,
+				token: token,
+				status: "You are successfully logged in!",
+			});
+		}
+	}
+);
+
+router.get(
 	"/",
 	cors.corsWithOptions,
 	authenticate.verifyUser,
